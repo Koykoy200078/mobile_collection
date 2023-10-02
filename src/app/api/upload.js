01a3b475b7f5cc311127e31b4f48a0ea1f7d8994
Koykoy200078 <franc200078@gmail.com>
2023-10-02 13:45:10 +0800

    update

--- src/app/api/upload.js
+++ src/app/api/upload.js
@@ -7,41 +7,41 @@ import databaseOptions, {
 import { showError, showInfo, showSuccess } from '../components/AlertMessage'
 import { Realm } from '@realm/react'
 
-const saveHistory = async (item) => {
-	try {
-		const realm = await Realm.open(databaseOptions)
-		realm.write(() => {
-			item.transactions.forEach((transaction) => {
-				const collectionReport = {
-					TRANSID: transaction.TRANSID,
-					TRANS_REFNO: transaction.TRANS_REFNO,
-					CLIENTID: transaction.CLIENTID,
-					CLIENT_NAME: transaction.CLIENT_NAME,
-					ACTUAL_PAY: transaction.ACTUAL_PAY,
-					TYPE_OF_PAYMENT: transaction.TYPE_OF_PAYMENT,
-					TRANS_DATETIME: transaction.TRANS_DATETIME,
-				}
-				console.log(JSON.stringify(collectionReport, null, 2))
-				realm.create(
-					CollectionReport,
-					collectionReport,
-					Realm.UpdateMode.Modified
-				)
-			})
-		})
+// const saveHistory = async (item) => {
+// 	try {
+// 		const realm = await Realm.open(databaseOptions)
+// 		realm.write(() => {
+// 			item.transactions.forEach((transaction) => {
+// 				const collectionReport = {
+// 					TRANSID: transaction.TRANSID,
+// 					TRANS_REFNO: transaction.TRANS_REFNO,
+// 					CLIENTID: transaction.CLIENTID,
+// 					CLIENT_NAME: transaction.CLIENT_NAME,
+// 					ACTUAL_PAY: transaction.ACTUAL_PAY,
+// 					TYPE_OF_PAYMENT: transaction.TYPE_OF_PAYMENT,
+// 					TRANS_DATETIME: transaction.TRANS_DATETIME,
+// 				}
+// 				console.log(JSON.stringify(collectionReport, null, 2))
+// 				realm.create(
+// 					CollectionReport,
+// 					collectionReport,
+// 					Realm.UpdateMode.Modified
+// 				)
+// 			})
+// 		})
 
-		showInfo({
-			message: 'Success',
-			description: 'Data saved successfully!',
-		})
-	} catch (error) {
-		showError({
-			message: 'Error',
-			description: 'Error updating data!',
-		})
-		console.error('Error: ', error)
-	}
-}
+// 		showInfo({
+// 			message: 'Success',
+// 			description: 'Data saved successfully!',
+// 		})
+// 	} catch (error) {
+// 		showError({
+// 			message: 'Error',
+// 			description: 'Error updating data!',
+// 		})
+// 		console.error('Error: ', error)
+// 	}
+// }
 
 export function* uploadDetails(payload) {
 	const { token } = yield select((state) => state.auth.authData.data)
@@ -61,9 +61,10 @@ export function* uploadDetails(payload) {
 			options
 		)
 		const data = yield response.json()
+		console.log(JSON.stringify(data, null, 2))
 		if (response.ok) {
 			// return data
-			saveHistory(data)
+			// saveHistory(data)
 
 			return data
 		} else {
